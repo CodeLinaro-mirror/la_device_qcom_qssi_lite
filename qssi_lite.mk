80a9c9ca758331fe25bfd8682154003858dbcf65
Arvind Kumar <quic_c_karvin@quicinc.com>
2022-05-04 21:51:56 +0530

    qssi_lite: Enable TARGET_HAS_LOW_RAM flag
    Change-Id: I88e76721426a5975553984711843c71b51d2b85a

--- qssi_lite.mk
+++ qssi_lite.mk
@@ -306,7 +306,7 @@ endif
 
 ##############################Go configs###########################################
 
-#TARGET_HAS_LOW_RAM := true
+TARGET_HAS_LOW_RAM := true
 
 # Enable DM file preopting to reduce first boot time
 PRODUCT_DEX_PREOPT_GENERATE_DM_FILES := true
